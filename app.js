83c8c3fe7cf8c9f7cd26e3b827e56b4e9c5d6042
Ryan Douglas <ryan@luckydogdigital.com>
2016-01-11 14:24:30 -0700

    cors work

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+// CORS Stuff
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+
 var express = require('express');
 var session = require('express-session')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var request = require('request');
+var FB = require('fb');
 var bodyParser = require('body-parser');
 
 var passport = require('passport');
@@ -22,13 +41,13 @@ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(allowCrossDomain);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session( {secret: process.env.SESSION_SECRET }));
 app.use(passport.initialize());
-// app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Passport
@@ -46,7 +65,7 @@ passport.use(new FacebookStrategy({
       // represent the logged-in user.  In a typical application, you would want
       // to associate the Facebook account with a user record in your database,
       // and return that user instead.
-      console.log(profile);
+      console.log('Name: ', profile.displayName);
       return done(null, profile);
     });
   }
@@ -70,17 +89,38 @@ passport.deserializeUser(function(id, done) {
 app.use('/', routes);
 app.use('/users', users);
 
+FB.api('oauth/access_token', {
+    client_id: 'app_id',
+    client_secret: 'app_secret',
+    grant_type: 'client_credentials'
+}, function (res) {
+    if(!res || res.error) {
+        console.log(!res ? 'error occurred' : res.error);
+        return;
+    }
+
+    var accessToken = res.access_token;
+});
+
+// FB.setAccessToken('access_token');
+// var accessToken = FB.getAccessToken();
+// console.log(accessToken);
+
 app.get('/auth/facebook',
   passport.authenticate('facebook'),
   function(req,res) {
+
 });
 
 app.get('/auth/facebook/callback',
   passport.authenticate('facebook', { failureRedirect: '/' }),
   function(req, res) {
     // Successful authentication, redirect home.
+    console.log("Code = ", req.query.code);
     res.redirect('/');
   });
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
