d8d352190e5fd0305e0f6251179054c22c8911ed
Ryan Douglas <ryan@luckydogdigital.com>
2016-01-12 15:59:10 -0700

    ts for heroku

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if (user) {
-    res.render('index', { title: 'Dander' });
-  } else {
     res.render('index', { title: 'Dander'})
   }
 });
