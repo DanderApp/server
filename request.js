3a032d3a16ad8f6884da2fc0df6e3551cb3d8672
Ryan Douglas <ryan@luckydogdigital.com>
2016-01-12 12:32:02 -0700

    moved call into root dir. Added dotenv

--- request.js
+++ request.js
@@ -1,9 +1,13 @@
 var request = require('request');
 var parseString = require('xml2js').parseString;
 
+require('dotenv').load()
+
 var requestFunction = function() {
 
-  request('http://api.petfinder.com/pet.getRandom?key=7e22a230c9629845bcfcad9d236be656&callback=?&output=basic', function(error, response, body){
+  request('http://api.petfinder.com/pet.getRandom?key=' +
+  process.env.PF_Key +
+  '&callback=?&output=basic', function(error, response, body){
   if(!error && response.statusCode==200){
     parseString(response.body, function(err, result){
       JSON.stringify(result);
