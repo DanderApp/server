96108254a0e7fae4b8b956d8913255f8622efa58
dlinch <dlinch33@gmail.com>
2016-01-14 16:14:17 -0700

    when passed a user.id, the server returns a list of dogs you like by calling the petfinder api and formatting the data into the json format the client side needs

--- filter.js
+++ filter.js
@@ -22,7 +22,29 @@ function filterDogs(responseData) {
 }
 
 
+function filterReturnArray(responseData) {
+  return new Promise(function(resolve, reject) {
+    var returnData = [];
+    for(i=0; i<responseData.length; i++){
+      if(responseData[i].petfinder.pet){
+      var data = responseData[i].petfinder.pet[0]
+        var newDog          = {};
+        newDog.name         = data.name[0];
+        newDog.age          = data.age[0];
+        newDog.sex          = data.sex[0];
+        newDog.size         = data.size[0];
+        newDog.description  = data.description[0];
+        newDog.petfinder_id = data.id[0];
+        newDog.photo        = data.media[0].photos[0].photo[2]["_"];
+        console.log(data.media)
+        returnData.push(newDog)
+      }
+    }
+    resolve(returnData);
+  });
+}
 
 module.exports = {
-  filter: filterDogs
+  filter: filterDogs,
+  format: filterReturnArray
 }
