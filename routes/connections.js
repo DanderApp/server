06addc3061f07738f2262de9cf21ccf49ba9ddeb
Ryan Douglas <ryan@luckydogdigital.com>
2016-01-14 16:17:49 -0700

    troubleshooting

--- routes/connections.js
+++ routes/connections.js
@@ -9,12 +9,14 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
 //works
 router.post('/new', function(req, res){
-  console.log(req.user.first_name + ' ' + req.user.last_name + ' has interacted with pet ' +  req.body.petfinder_id)
-  crud.Connection.createConnection(req.user.id, req.body.petfinder_id, req.body.liked)
-  .then(function(data){
-    console.log(data);
-    res.json({id:data[0]});
-  })
+  console.log('Post accepted');
+  res.end();
+  // console.log(req.user.first_name + ' ' + req.user.last_name + ' has interacted with pet ' +  req.body.petfinder_id)
+  // crud.Connection.createConnection(req.user.id, req.body.petfinder_id, req.body.liked)
+  // .then(function(data){
+  //   console.log(data);
+  //   res.json({id:data[0]});
+  // })
 })
 
 // works
