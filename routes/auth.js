2bd36f4b977b8807519dcec07de32023205f6a0f
Ryan Douglas <ryan@luckydogdigital.com>
2016-01-14 12:06:48 -0700

    error handling

--- routes/auth.js
+++ routes/auth.js
@@ -82,7 +82,7 @@ router.post('/login',
 function(req,res,next) {
   passport.authenticate('local',
   function(err, user, info) {
-    if (err) return next(err);
+    // if (err) return next(err);
     if(user) {
       createToken(user).then(function(token) {
         res.json({
